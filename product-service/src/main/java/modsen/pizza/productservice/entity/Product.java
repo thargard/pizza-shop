ab9f92839ef4132aacfc6eaf15b5294fbee093c0
thargard <boris.margolin.1301@gmail.com>
2025-02-18 14:36:51 +0300

    Добавлена миграция бд через liquibase для таблицы продуктов

--- product-service/src/main/java/modsen/pizza/productservice/entity/Product.java
+++ product-service/src/main/java/modsen/pizza/productservice/entity/Product.java
@@ -1,15 +1,13 @@
 package modsen.pizza.productservice.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "products")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
