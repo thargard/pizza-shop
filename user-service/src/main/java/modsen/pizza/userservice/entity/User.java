2e29aba662bc0be21bc97d95c314d14a41ca30d7
thargard <boris.margolin.1301@gmail.com>
2025-02-20 22:00:19 +0300

    Добавлена базовая бизнес-логика для  микросервиса пользователей

---
+++ user-service/src/main/java/modsen/pizza/userservice/entity/User.java
@@ -0,0 +1,23 @@
+package modsen.pizza.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private String role;
+}
