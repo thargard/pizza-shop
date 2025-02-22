30e14d97be337a770211028a343de4d776660d15
thargard <boris.margolin.1301@gmail.com>
2025-02-22 19:13:38 +0300

    Добавлены изменения для работы аутентификации

--- product-service/src/main/java/modsen/pizza/productservice/controllers/ProductController.java
+++ product-service/src/main/java/modsen/pizza/productservice/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
     @Autowired
     private ProductService productService;
