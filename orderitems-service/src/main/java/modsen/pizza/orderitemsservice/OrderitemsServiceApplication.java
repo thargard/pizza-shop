db4ad1d932674c1f0c94f59b8b2135ed69d42d76
thargard <boris.margolin.1301@gmail.com>
2025-02-27 14:50:37 +0300

    orderitems-service init

---
+++ orderitems-service/src/main/java/modsen/pizza/orderitemsservice/OrderitemsServiceApplication.java
@@ -0,0 +1,13 @@
+package modsen.pizza.orderitemsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderitemsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderitemsServiceApplication.class, args);
+    }
+
+}
