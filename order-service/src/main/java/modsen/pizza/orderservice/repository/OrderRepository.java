f8224e557fa16b34cf48891cf9cefeb2192180a4
thargard <boris.margolin.1301@gmail.com>
2025-02-27 14:45:41 +0300

    добавлена основная бизнес логика для сервиса заказов, а также миграция для бд

---
+++ order-service/src/main/java/modsen/pizza/orderservice/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package modsen.pizza.orderservice.repository;
+
+import modsen.pizza.orderservice.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
