901de3c9e02f4fbbda0da9509965634ec441bbe0
thargard <boris.margolin.1301@gmail.com>
2025-03-04 16:25:04 +0300

    Добавлен класс обработчика ошибок, исправлены мелкие ошибки

--- new-service/src/main/java/modsen/pizza/newservice/dto/UserDto.java
+++ new-service/src/main/java/modsen/pizza/newservice/dto/UserDto.java
@@ -18,4 +18,36 @@ public class UserDto {
     @NotBlank(message = "Username shouldn't be empty!")
     private String username;
     private String role;
+
+    public @NotBlank(message = "Email shouldn't be empty!") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "Email shouldn't be empty!") String email) {
+        this.email = email;
+    }
+
+    public @NotBlank(message = "Password shouldn't be empty!") String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotBlank(message = "Password shouldn't be empty!") String password) {
+        this.password = password;
+    }
+
+    public @NotBlank(message = "Username shouldn't be empty!") String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NotBlank(message = "Username shouldn't be empty!") String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
